44057d678695b451cc8986d7f5638327534e603e
unknown <kevinmarsha131@gmail.com>
2024-09-06 10:45:29 +0700

    first github project

---
+++ Luas_Segitiga.java
@@ -0,0 +1,15 @@
+public class Luas_Segitiga {
+
+    public static void main (String[] args) {
+        // deklarasi variabel
+        double alas, tinggi, luas;
+
+        // inisialisasi variabel
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+
+        //cetak hasil
+        System.out.println("luas segitiga adalah " +luas);
+    }
+}
